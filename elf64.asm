7be621cfec0c76789cadd40ebab49ab1720d6bd6
Fernando Naranjo Talavera <73451380+efejjota@users.noreply.github.com>
2022-07-06 15:14:20 +0000

    Test assembling for Macho64, Win64 and Elf64 (#2)
    * Rename files to allow testing other systems
    
    * Fix nasm command specified in assembly file
    
    * Test assembling and running in Macos64
    
    * Test assembling and running in Windows64
    
    * Fix object format extension in nasm command
    
    * Rewrite Windows test in powershell syntax
    
    GHActions runs powershell by default for windows runners
    
    * List files in current dir and try to execute .\a.exe
    
    * Optimise workflows to only run when relevant files change
    
    * Stop listing directory contents in Windows test

--- elf64.asm
+++ elf64.asm
@@ -2,7 +2,7 @@
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
 ; To assemble and run:
 ;
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+;     nasm -felf64 elf64.asm && ld elf64.o && ./a.out
 ; ----------------------------------------------------------------------------------------
 
           global    _start
